4356ca10c7fcf7a7824489a4d159e2db62e82f3b
sanddot <sanginta@gmail.com>
2024-08-08 20:04:07 -0700

    Added search function with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function searchCity(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = document.querySelector("#current-city");
-  city.innerHTML = searchInputElement.value;
+function updateWeather(response) {
+  let currentTemperature = document.querySelector("#temperature");
+  let responseTemperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  currentTemperature.innerHTML = responseTemperature;
 }
 
+function searchCity(city) {
+  let apiKey = "t3f3af9b184481d0306edc82cbo6ff8c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  searchCity(searchInputElement.value);
+}
 let enterCityForm = document.querySelector("#search-city-form");
-enterCityForm.addEventListener("submit", searchCity);
+enterCityForm.addEventListener("submit", search);
+
+searchCity("Kelowna");
